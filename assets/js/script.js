3d3d8fa1ab624ab76ea4a9d11fb1de6d86a64fbf
Ethan Willers <willers.enw@gmail.com>
2022-11-28 14:18:27 -0800

    finished five day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,26 +7,31 @@ $(() => {
     console.log(fiveDayEl);
 
     function setWeatherData(weather, El) {
-        var iconUrl = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+        
 
         if (El.attr('id') === 'current-weather') {
+            var iconUrl = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
             El.children().eq(0).text(weather.name + ' ' + dayjs(weather.dt * 1000).format('MM/DD/YYYY'));
             El.children().eq(1).attr('src', iconUrl);
             El.children().eq(2).text('Temp: ' + weather.main.temp + '°F');
             El.children().eq(3).text('Wind: ' + weather.wind.speed + ' MPH');
             El.children().eq(4).text('Humidity: ' + weather.main.humidity + ' %');
         } else {
-            El.children().eq(0).text(dayjs(weather.dt * 1000).format('MM/DD/YYYY'));
+            El.children().eq(0).text(dayjs(weather.date * 1000).format('MM/DD/YYYY'));
+            El.children().eq(1).text('Temp: ' + weather.temp + '°F');
+            El.children().eq(2).text('Wind: ' + weather.wind + ' MPH');
+            El.children().eq(3).text('Humidity: ' + weather.humidity + ' %');
         }
     }
 
     function averageData(list) {
         console.log(list);
+        var fiveDayAverages= []
         var start = 0;
         var end = 8;
 
         for (var i = 0; i < 5; i++) {
-            var averages = {
+            var average = {
                 temp: 0,
                 wind: 0,
                 humidity: 0,
@@ -34,20 +39,24 @@ $(() => {
             }
 
             for (var x = start; x < end; x++) {
-                averages.temp += list[x].main.temp;
-                averages.wind += list[x].wind.speed;
-                averages.humidity += list[x].main.humidity;
+                average.temp += list[x].main.temp;
+                average.wind += list[x].wind.speed;
+                average.humidity += list[x].main.humidity;
             }
             
 
-            averages.temp = averages.temp / 8;
-            averages.wind = averages.wind / 8;
-            averages.humidity = averages.humidity / 8;
-            averages.date = list[start].dt;
-            console.log(averages);
+            average.temp = Math.round(average.temp / 8);
+            average.wind = Math.round(average.wind / 8);
+            average.humidity = Math.round(average.humidity / 8);
+            average.date = list[start].dt;
+            
+            fiveDayAverages[i] = average;
+
             start += 8;
             end += 8;
         }
+        
+        return fiveDayAverages;
     }
 
     function getWeatherData5Day(lat, lon) {
@@ -58,11 +67,10 @@ $(() => {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data);
-                averageData(data.list);
-                // for (var x = 0; x < 5; x++) {
-                //     // setweather()
-                // }
+                var forcast = averageData(data.list);
+                for (var x = 0; x < 5; x++) {
+                    setWeatherData(forcast[x], fiveDayEl.children().eq(x));
+                }
             });
     }
 
