fde8aaf92700a4e60baa0babb0634d4bf961cf31
Ethan Willers <willers.enw@gmail.com>
2022-11-27 23:20:23 -0800

    fetch for geocoding api added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,22 @@
 $(() => {
-    
+    var searchButton = $('#search-btn');
+    var searchForm = $('#search-form');
+
+    function getGeoData(city) {
+        var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=a6e98ccb06f33151ebb17a9d35aa5234';
+        fetch(requestUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            });
+    }
+
+    searchButton.click(function (e) {
+        e.preventDefault();
+        var cityName = searchForm.children('input').val().trim();
+        console.log(cityName);
+        getGeoData(cityName);
+    });
 });
