cd780d95ec96b898749ab0384055b078ee154f2c
Ethan Willers <willers.enw@gmail.com>
2022-11-28 01:50:02 -0800

    added function to load data on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,49 @@
 $(() => {
     var searchButton = $('#search-btn');
     var searchForm = $('#search-form');
+    var currentWeatherEl = $('#current-weather');
+    var fiveDayEl = $('#5-day').children().eq(1);
 
-    function getWeatherData(lat, lon) {
-        var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' + lat + '&lon=' + lon +'&appid=a6e98ccb06f33151ebb17a9d35aa5234';
+    console.log(fiveDayEl);
+
+    function setWeatherData(weather, El) {
+        var iconUrl = "http://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
+
+        if (El.attr('id') === 'current-weather') {
+            El.children().eq(0).text(weather.name + ' ' + dayjs().format('MM/DD/YYYY'));
+        } else {
+            El.children().eq(0).text()
+        }
+
+        El.children().eq(1).attr('src', iconUrl);
+        El.children().eq(2).text('Temp: ' + weather.main.temp + '°F');
+        El.children().eq(3).text('Wind: ' + weather.wind.speed + ' MPH');
+        El.children().eq(4).text('Humidity: ' + weather.main.humidity + ' %');
+    }
+
+    function getWeatherData5Day(lat, lon) {
+        var requestUrl = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=a6e98ccb06f33151ebb17a9d35aa5234';
+
         fetch(requestUrl)
             .then(function (response) {
                 return response.json();
             })
             .then(function (data) {
                 console.log(data);
+            });
+    }
+
+    function getWeatherData(lat, lon) {
+        var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' + lat + '&lon=' + lon + '&appid=a6e98ccb06f33151ebb17a9d35aa5234';
+
+        fetch(requestUrl)
+            .then(function (response) {
+                return response.json();
             })
+            .then(function (data) {
+                console.log(data);
+                setWeatherData(data, currentWeatherEl);
+            });
     }
 
     function getGeoData(city) {
@@ -22,6 +55,7 @@ $(() => {
             .then(function (data) {
                 console.log(data);
                 getWeatherData(data[0].lat, data[0].lon);
+                getWeatherData5Day(data[0].lat, data[0].lon);
             });
     }
 
