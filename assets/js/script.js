c0a8cb252574f829d345c8c22ffe4c546e0bb30a
Ethan Willers <willers.enw@gmail.com>
2022-11-27 23:47:43 -0800

    added fetch for weather data

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,17 @@ $(() => {
     var searchButton = $('#search-btn');
     var searchForm = $('#search-form');
 
+    function getWeatherData(lat, lon) {
+        var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' + lat + '&lon=' + lon +'&appid=a6e98ccb06f33151ebb17a9d35aa5234';
+        fetch(requestUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data);
+            })
+    }
+
     function getGeoData(city) {
         var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=a6e98ccb06f33151ebb17a9d35aa5234';
         fetch(requestUrl)
@@ -10,6 +21,7 @@ $(() => {
             })
             .then(function (data) {
                 console.log(data);
+                getWeatherData(data[0].lat, data[0].lon);
             });
     }
 
