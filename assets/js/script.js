79c442ca38fa947f6f60d1429a37211c4607154d
Ethan Willers <willers.enw@gmail.com>
2022-11-29 23:32:57 -0800

    filled readme and commented on javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,18 @@
 $(() => {
+    // Doc quereys
     var searchButton = $('#search-btn');
     var searchForm = $('#search-form');
     var currentWeatherEl = $('#current-weather');
     var fiveDayEl = $('#5-day').children().eq(1);
     var savedSearches = $('#previous-searches');
 
+    //Global variables
     var cityButtonLimit = 5;
 
-    console.log(fiveDayEl);
-
     function setWeatherData(weather, El) {
+        // Loads weather info onto page
         if (El.attr('id') === 'current-weather') {
+            // Current weather
             var iconUrl = "https://openweathermap.org/img/w/" + weather.weather[0].icon + ".png";
             El.children().eq(0).text(weather.name + ' ' + dayjs(weather.dt * 1000).format('MM/DD/YYYY'));
             El.children().eq(1).attr('src', iconUrl);
@@ -19,6 +21,7 @@ $(() => {
             El.children().eq(4).text('Humidity: ' + weather.main.humidity + ' %');
 
         } else {
+            // Five day forecast
             var iconUrl = "https://openweathermap.org/img/w/" + weather.icon + ".png";
             El.children().eq(0).text(dayjs(weather.date * 1000).format('MM/DD/YYYY'));
             El.children().eq(1).attr('src', iconUrl);
@@ -28,12 +31,14 @@ $(() => {
         }
     }
 
+    // Takes a array of 40 3-hour weather intervals and gets the average weather for each day
     function averageData(list) {
         console.log(list);
         var fiveDayAverages = []
         var start = 0;
         var end = 8;
 
+        // A nested for loop to sort 40 3-hour weather intervals
         for (var i = 0; i < 5; i++) {
             var average = {
                 temp: 0,
@@ -49,23 +54,24 @@ $(() => {
                 average.humidity += list[x].main.humidity;
             }
 
-
+            // Gets the average for each piece of weather info and rounds each value
             average.temp = Math.round(average.temp / 8);
             average.wind = Math.round(average.wind / 8);
             average.humidity = Math.round(average.humidity / 8);
             average.date = list[start].dt;
             average.icon = list[end - 4].weather[0].icon;
 
+            // Fills an array with our averages
             fiveDayAverages[i] = average;
 
             start += 8;
             end += 8;
         }
-        console.log(fiveDayAverages);
 
         return fiveDayAverages;
     }
 
+    // Fetch request for the five day weater forecast
     function getWeatherData5Day(lat, lon) {
         var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=' + lat + '&lon=' + lon + '&appid=a6e98ccb06f33151ebb17a9d35aa5234';
 
@@ -81,6 +87,7 @@ $(() => {
             });
     }
 
+    // Fetch request for the current weather data
     function getWeatherData(lat, lon) {
         var requestUrl = 'https://api.openweathermap.org/data/2.5/weather?units=imperial&lat=' + lat + '&lon=' + lon + '&appid=a6e98ccb06f33151ebb17a9d35aa5234';
 
@@ -89,11 +96,11 @@ $(() => {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data);
                 setWeatherData(data, currentWeatherEl);
             });
     }
 
+    // Fetch request for the latitude and longitude of the inputed city
     function getGeoData(city) {
         var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + city + '&limit=1&appid=a6e98ccb06f33151ebb17a9d35aa5234';
         fetch(requestUrl)
@@ -101,7 +108,6 @@ $(() => {
                 return response.json();
             })
             .then(function (data) {
-                console.log(data);
                 // Checks for empty data
                 if (data.length === 0) {
                     currentWeatherEl.children().eq(0).text('Not a valid city name!');
@@ -109,7 +115,7 @@ $(() => {
                     return;
                 }
 
-                // Checks to prevent duplicates
+                // Checks to prevent duplicate buttons
                 var duplicate = false;
                 for (var x = 0; x < savedSearches.children().length; x++) {
                     if (savedSearches.children().eq(x).text() === city) {
@@ -117,7 +123,7 @@ $(() => {
                     }
                 }
 
-                // Creates a new element based off user searches
+                // Creates a new element based off user searches if no duplicates are found
                 if (!duplicate) {
                     var searchButton = $('<button>');
                     searchButton.attr('class', 'btn btn-secondary text-black w-100 mb-3');
@@ -136,6 +142,7 @@ $(() => {
             });
     }
 
+    // Checks for click events on the search button
     searchButton.click(function (e) {
         e.preventDefault();
         var cityName = searchForm.children('input').val().trim();
@@ -147,6 +154,7 @@ $(() => {
         getGeoData(cityName);
     });
 
+    // Checks for submit events on the input form
     searchForm.submit(function (e) {
         e.preventDefault();
         var cityName = searchForm.children('input').val().trim();
@@ -158,6 +166,7 @@ $(() => {
         getGeoData(cityName);
     });
 
+    // Checks for click events on the previous search buttons
     savedSearches.click(function (e) {
         e.preventDefault();
         var element = e.target;
